47ee27ee5d3e2bacd46249304013482281ed804d
yashigani <tai.fukui@gmail.com>
2015-03-05 22:15:00 +0900

    Initial Commit

---
+++ WebKitPlus/WebKitPlus.h
@@ -0,0 +1,19 @@
+//
+//  WebKitPlus.h
+//  WebKitPlus
+//
+//  Created by taiki on 3/5/15.
+//  Copyright (c) 2015 yashigani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebKitPlus.
+FOUNDATION_EXPORT double WebKitPlusVersionNumber;
+
+//! Project version string for WebKitPlus.
+FOUNDATION_EXPORT const unsigned char WebKitPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebKitPlus/PublicHeader.h>
+
+
