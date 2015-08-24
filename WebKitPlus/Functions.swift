5141a4ada9136cfa4315e3b1c3f5c75f51237c76
yashigani <tai.fukui@gmail.com>
2015-08-24 17:55:40 +0900

    Ignore server trust
    `webView(_:, didReceiveAuthenticationChallenge:, completionHandler)` called by https in iOS 9.

--- WebKitPlus/Functions.swift
+++ WebKitPlus/Functions.swift
@@ -1,23 +1,29 @@
 import UIKit
 
-public func alertForAuthentication(challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential!) -> Void) -> UIAlertController {
+public func alertForAuthentication(challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential!) -> Void) -> UIAlertController? {
     let space = challenge.protectionSpace
-    let alert = UIAlertController(title: "\(space.`protocol`!)://\(space.host):\(space.port)", message: space.realm, preferredStyle: .Alert)
-    alert.addTextFieldWithConfigurationHandler {
-        $0.placeholder = localizedString("user")
-    }
-    alert.addTextFieldWithConfigurationHandler {
-        $0.placeholder = localizedString("password")
-        $0.secureTextEntry = true
-    }
-        alert.addAction(UIAlertAction(title: localizedString("Cancel"), style: .Cancel) { _ in
+    let alert: UIAlertController?
+    if space.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+        // ignore Server Trust.
+        alert = nil
+    } else {
+        alert = UIAlertController(title: "\(space.`protocol`!)://\(space.host):\(space.port)", message: space.realm, preferredStyle: .Alert)
+        alert?.addTextFieldWithConfigurationHandler {
+            $0.placeholder = localizedString("user")
+        }
+        alert?.addTextFieldWithConfigurationHandler {
+            $0.placeholder = localizedString("password")
+            $0.secureTextEntry = true
+        }
+        alert?.addAction(UIAlertAction(title: localizedString("Cancel"), style: .Cancel) { _ in
             completionHandler(.CancelAuthenticationChallenge, nil)
         })
-        alert.addAction(UIAlertAction(title: localizedString("OK"), style: .Default) { _ in
-            let textFields = alert.textFields as! [UITextField]
-            let credential = NSURLCredential(user: textFields[0].text, password: textFields[1].text, persistence: .ForSession)
+        alert?.addAction(UIAlertAction(title: localizedString("OK"), style: .Default) { _ in
+            let textFields = alert!.textFields as! [UITextField]
+            let credential = NSURLCredential(user: textFields[0].text!, password: textFields[1].text!, persistence: .ForSession)
             completionHandler(.UseCredential, credential)
         })
+    }
     return alert
 }
 
