df26620bce8c4b10495cae6b2753efee4e47b6cb
yutailang0119 <yutailang0119@gmail.com>
2022-01-14 12:09:38 +0900

    swift package init --type library

---
+++ Sources/WebKitPlus/WebKitPlus.swift
@@ -0,0 +1,6 @@
+public struct WebKitPlus {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/WebKitPlusTests/WebKitPlusTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WebKitPlus
+
+final class WebKitPlusTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WebKitPlus().text, "Hello, World!")
+    }
+}
