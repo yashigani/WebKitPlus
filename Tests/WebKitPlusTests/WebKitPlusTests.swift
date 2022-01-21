7eb133cb10c16451740923a19364d22e8cb99694
yutailang0119 <yutailang0119@gmail.com>
2022-01-21 22:20:31 +0900

    Fix to remove WebKitPlusTests

--- Tests/WebKitPlusTests/WebKitPlusTests.swift
+++ Tests/WebKitPlusTests/WebKitPlusTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class WebKitPlusTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(WebKitPlus().text, "Hello, World!")
+        XCTAssert(true, "Pass")
     }
 }
